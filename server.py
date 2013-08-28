3f72ee6d4b6170f1b165f01f9ba9703a472ef3a4
Mark Goodwin <mgoodwin@mozilla.com>
2013-08-28 10:03:35 +0100

    some changes, apparently

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import BaseHTTPServer
 import os.path
 import json
+import cgi
 from string import Template
 
 def run(server_class=BaseHTTPServer.HTTPServer,
@@ -37,19 +38,20 @@ def handle(s, path, prefix, template):
   s.wfile.write('not found')
 
 def makeHandler(prefix, template):
-  def handler(s, path):
+  def handler(s, path, postvars):
     return handle(s, path, prefix, template)
   return handler
 
-def handleCall(s, path):
+def handleCall(s, path, postvars):
   s.send_response(200)
   s.send_header('Content-type','application/json');
   s.end_headers()
   headers = {}
   for header in s.headers:
     headers[header] = s.headers[header]
-  dic = {'path':s.path,'headers':headers,'Result':'Seems to work: '+s.path}
+  dic = {'path':s.path,'headers':headers,'Result':'Seems to work: '+s.path, 'postvars':postvars}
   data = json.dumps(dic)
+  print 'data is '+data
   s.wfile.write(data)
 
 routes = {'/static/':makeHandler(prefix = staticPrefix, template = False),
@@ -61,7 +63,24 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     """Respond to a GET request"""
     for route in routes.keys():
       if s.path.startswith(route):
-        return routes[route](s,s.path[len(route):])
+        return routes[route](s,s.path[len(route):],{})
+    s.send_response(200)
+    s.send_header('Content-type','text/html')
+    s.end_headers()
+    s.wfile.write('<html><body><p>maybe you want <a href="/static/docs/api/allclasses-noframe.html">docs</a></body></html>')
+
+  def do_POST(s):
+    ctype, pdict = cgi.parse_header(s.headers.getheader('content-type'))
+    if ctype == 'multipart/form-data':
+        postvars = cgi.parse_multipart(s.rfile, pdict)
+    elif ctype == 'application/x-www-form-urlencoded':
+        length = int(s.headers.getheader('content-length'))
+        postvars = cgi.parse_qs(s.rfile.read(length), keep_blank_values=1)
+    else:
+        postvars = {}
+    for route in routes.keys():
+      if s.path.startswith(route):
+        return routes[route](s,s.path[len(route):],postvars)
     s.send_response(200)
     s.send_header('Content-type','text/html')
     s.end_headers()
